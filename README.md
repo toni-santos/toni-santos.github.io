ea01679b8c9140ecf6213b4fcf95a0e76edffbaf
toni-santos <a.oliveirasantos122@gmail.com>
2021-08-22 04:43:07 +0100

    feat: first launch :)

--- README.md
+++ README.md
@@ -3,3 +3,14 @@
 simple introductory page
 
 using the library <a href="https://github.com/Prinzhorn/skrollr">skrollr</a> to achieve the horizontally scrolling effect
+
+# TODO:
+
+[x] somewhat functional page :)
+[x] implement horizontal scrolling
+[] responsiveness (\@media queries)
+[] menu for easier browsing
+[] projects page (haven't done enough yet :/)
+[] glowing text
+[] minor animations
+[] glowing socials buttons
--- scripts.js
+++ scripts.js
@@ -1,11 +1,24 @@
-let div = document.getElementsByClassName("tech-item")
+let className = document.getElementsByClassName("tech-item")
+const langsNum = 6
+const beginnerLangs = ["technologies-js", "technologies-node"]
 
-// div.item thing to replace the functions beneath
-
-div.addEventListener("mouseenter", function(event) {
-    div.innerHTML = "Intermediate"
-}, false)
-
-div.addEventListener("mouseleave", function(event){
-    div.innerHTML = ""
-}, false)
+window.setInterval(function(){
+    for (let i=0; i < langsNum; i++) {
+        let div = className.item(i)
+        if (beginnerLangs.includes(div.id)){
+            div.addEventListener("mouseenter", function(event) {
+                div.innerHTML = '<p id="langs-proficiency">Beginner</p>'
+            }, false)
+            div.addEventListener("mouseleave", function(event) {
+                div.innerHTML = ""
+            }, false)
+        } else {
+            div.addEventListener("mouseenter", function(event) {
+                div.innerHTML = '<p id="langs-proficiency">Intermediate</p>'
+            }, false)
+            div.addEventListener("mouseleave", function(event) {
+                div.innerHTML = ""
+            }, false)
+        }
+    }    
+}, 500);
