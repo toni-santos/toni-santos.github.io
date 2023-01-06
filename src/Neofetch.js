12e3aca2e0b80d1d6648f98cb8a9c413f12e4acc
toni-santos <a.oliveirasantos122@gmail.com>
2023-01-06 00:08:21 +0000

    fixed website and some updates

--- src/Neofetch.js
+++ src/Neofetch.js
@@ -8,13 +8,13 @@ function Neofetch() {
     const [img, setImg] = useState(me)
 
     useEffect(() => {
-        if ( count == 100 ) setImg(swag)
-        }, [count, setImg, swag]);
+        if ( count === 100 ) setImg(swag)
+        }, [count, setImg]);
     
     return(
         <>
             <div id="neoWrapper">
-                <img id="imgMe" src={img}  onClick={() => setCount(count + 1)} draggable="false" />
+                <img id="imgMe" alt="profile" src={img}  onClick={() => setCount(count + 1)} draggable="false" />
                 <div id="neoDesc">
                     <ul className="neoUl">
                         <li>pronouns: any</li>
@@ -23,7 +23,7 @@ function Neofetch() {
                         <li>studying: informatics and computation</li>
                         <li>@: feup</li>
                         <li>year: 3rd</li>
-                        <li>interests: <a href="https://www.last.fm/user/n4gi7o" target="_blank">music</a> mmorpgs photography & game development</li>
+                        <li>interests: <a href="https://www.last.fm/user/n4gi7o" rel="noreferrer" target="_blank">music</a> mmorpgs photography & game development</li>
                         <li>technologies: html css js figma react python c++</li>
                     </ul>
                 </div>
--- src/Projects.js
+++ src/Projects.js
@@ -4,9 +4,9 @@ function Projects() {
             <p id="desc">Some personal and uni related projects I've done in the past!</p>
             <ul id="projli">
                 <li><a href="https://github.com/toni-santos/toni-santos.github.io" target="_blank" rel="noreferrer" >Personal website</a></li>
-                <li><a href="https://github.com/toni-santos/sttpg" target="_blank" rel="noreferrer" >Personal startpage</a></li>
                 <li><a href="https://github.com/toni-santos/ldts-proj" target="_blank" rel="noreferrer" >Into the Breach Clone/Demake for Uni course</a></li>
                 <li><a href="https://github.com/toni-santos/ltw-proj" target="_blank" rel="noreferrer" >Website built with HTML/CSS/PHP</a></li>
+                <li><a href="https://github.com/toni-santos/lbaw-earworm" target="_blank" rel="noreferrer" >Website built with Laravel</a></li>
             </ul>
         </>
     )
